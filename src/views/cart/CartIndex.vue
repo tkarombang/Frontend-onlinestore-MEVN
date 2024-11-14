34f393aa9364a4d0ab3fe479b1863b09cedf7668
tkarombang <tuangkarombang@gmail.com>
2024-11-15 00:10:48 +0800

    Styling in Cart Page and total price currency IDR

--- src/views/cart/CartIndex.vue
+++ src/views/cart/CartIndex.vue
@@ -22,23 +22,27 @@
                 <h6 class="title is-6">{{ item.name }}</h6>
                 <p class="subtitle is-size-6">{{ item.price }}</p>
                 <p class="subtitle is-size-6">{{ item.description }}</p>
-
-                <div class="is-flex">
-                  <i class="fa-regular fa-star column is-0"></i>
-                  <h5 class="column">ratings</h5>
-                </div>
-
-
               </div>
+
+              <div class="card-footer">
+                <div class="card-footer-item is-flex is-justify-content-end">
+                  <i class="fa-regular fa-star is-0"></i>
+                  <h5 class="ml-2">ratings</h5>
+                </div>
+              </div>
+
             </div>
 
           </div>
 
-          <div class="column is-narrow ">
+          <div class="column is-narrow plex">
             <div class="content-flex">
               <button class="button is-info is-small" style="width: 34px;">+</button>
               <input type="text" class="input is-small" style="width: 34px;">
               <button class="button is-danger is-small" style="width: 34px;">-</button>
+            </div>
+            <div>
+              <button class="button">Remove</button>
             </div>
 
           </div>
@@ -49,7 +53,10 @@
 
       </div>
       <div>
-        <h1>Total: Rpxxxxxx</h1>
+        <h1 class="title is-5">Total: {{ new Intl.NumberFormat('id-ID', {
+          style: 'currency',
+          currency: 'IDR'
+        }).format(totalPrice) }}</h1>
       </div>
     </div>
   </div>
@@ -60,15 +67,23 @@
 import { computed, ref } from 'vue';
 import { cartItems } from '@/data-seed';
 
+// DATA UNTUK LATIHAN
 const dataCart = ref(cartItems)
-dataCart.value.forEach(element => {
-  console.log(element.name)
-});
+// dataCart.value.forEach(element => {
+//   // console.log(element.price)
+//   const potong = parseFloat(element.price.split('.').join(''))
+//   const formatter = new Intl.NumberFormat('id-ID').format(potong,)
+//   console.log(formatter)
+
+// });
 
 const totalPrice = computed(() => {
-  return cartItems.reduce((total, item) => total + Number(item.price), 0)
+  return cartItems.reduce((total, item) =>
+    total + parseInt(item.price.split('.').join('')), 0
+    // new Intl.NumberFormat('id-ID').format(total + parseInt(item.price),), 0
+  )
 })
-console.log(totalPrice.value)
+console.log(new Intl.NumberFormat('id-ID', {}).format(totalPrice.value),)
 
 </script>
 
@@ -80,10 +95,22 @@ console.log(totalPrice.value)
   gap: 5px
 }
 
+.plex {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
 /* MOBILE DISPLAY */
 @media only screen and (max-width: 768px) {
   .content-flex {
     flex-direction: row;
+  }
+
+  .plex {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
   }
 }
 </style>
