aaa2f6ec4af55e8a0ff42b5bcd801a6ee8dd6615
tkarombang <tuangkarombang@gmail.com>
2024-11-25 11:50:15 +0800

    Add Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,6 @@
 <template>
   <div id="app">
-
-    <nav class="navbar is-primary has-shadow title is-size-4" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-
-        <h1 class="navbar-item ">ONLINESHOP</h1>
-
-        <a role="button" class="navbar-burger has-background-primary-10 has-text-primary-10-invert"
-          data-target="navMenu" aria-label="menu" aria-expanded="false">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-      <div class="navbar-menu" id="navMenu">
-        <!-- navbar start, navbar end -->
-        <div class="navbar-start">
-          <!-- navbar items -->
-          <RouterLink to="/product" class="navbar-item">Product</RouterLink>
-          <!-- <RouterLink :to="{ name: 'Detail', params: { id: 123 } }" class="navbar-item">Product Detail</RouterLink> -->
-          <RouterLink to="/cart" class="navbar-item">Cart</RouterLink>
-        </div>
-
-        <div class="navbar-end">
-          <!-- navbar items -->
-          <div class="navbar-item">
-            <div class="field is-grouped">
-              <p class="control">
-                <a class="button">
-                  <span class="icon">
-                    <i class="fas fa-twitter" aria-hidden="true"></i>
-                  </span>
-                  <span>Tweet</span>
-                </a>
-              </p>
-              <p class="control">
-                <a class="button is-primary">
-                  <span class="icon">
-                    <i class="fas fa-download" aria-hidden="true"></i>
-                  </span>
-                  <span>Download</span>
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-
-
-
-
-
+    <NavBar />
     <main>
       <router-view></router-view>
     </main>
@@ -61,7 +9,7 @@
 
 <script setup>
 import { RouterView } from "vue-router";
-import { RouterLink } from "vue-router";
+import NavBar from "./components/NavBar.vue";
 
 document.addEventListener('DOMContentLoaded', () => {
 
